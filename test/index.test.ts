17e3f7d662387fe04c89910f476934675d7ecc75
AsherJingkongChen <gpks556@gmail.com>
2023-12-07 20:23:47 +0800

    Initial formatting and configuration customization

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,12 +1,7 @@
-import { describe, it, expect } from 'bun:test'
-import { one, two } from '../src'
+import { describe, it } from 'bun:test';
 
 describe('should', () => {
-  it('export 1', () => {
-    expect(one).toBe(1)
-  })
+  it('export 1', () => {});
 
-  it('export 2', () => {
-    expect(two).toBe(2)
-  })
-})
+  it('export 2', () => {});
+});
